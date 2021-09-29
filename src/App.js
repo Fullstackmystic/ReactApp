a0113d2a1adcbc138c1a4f42e51149d4a400a971
Prateek Sogra <80418551+Fullstackmystic@users.noreply.github.com>
2021-09-30 02:03:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from "./Components/Navbar.js";
+import Button from "./Components/Button.js"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello</h1>
+       <Button/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
